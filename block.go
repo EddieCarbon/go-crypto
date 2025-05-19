54467568fe1e983519dbf93ec5ba9d24769913dd
Kuba Tomiczek <92593243+EddieCarbon@users.noreply.github.com>
2025-05-19 02:07:31 +0200

    Feature: Transaction (#12)
    * Implements basic blockchain structure
    
    Introduces the basic structure of a blockchain, including blocks,
    transactions, and UTXO management.
    
    Adds functionality for creating a new blockchain, mining blocks,
    finding unspent transactions, and retrieving UTXOs for a given address.
    
    Integrates command-line interface for basic operations like creating
    a blockchain and checking account balance.
    
    Replaces block data with transactions and introduces coinbase transaction.
    
    * Adds functionality to find spendable outputs
    
    Implements `FindSpendableOutputs` function to identify and return
    unspent transaction outputs (UTXOs) that can be used as inputs
    for new transactions, enabling the creation of new transactions
    that spend existing UTXOs. This function accumulates UTXOs until
    the specified amount is reached.
    
    Also, the commit fixes an issue where the blockchain tip was
    incorrectly retrieved during blockchain initialization.
    
    * Adds send and printchain commands
    
    Implements the `send` command to allow users to send coins between addresses.
    
    Implements the `printchain` command to display the blocks in the blockchain.
    
    * Removes commented-out blockchain initialization
    
    Removes the commented-out blockchain initialization code,
    leaving only the CLI initialization. This cleans up the main
    function and ensures a cleaner execution flow.

--- block.go
+++ block.go
@@ -2,34 +2,19 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"encoding/gob"
+	"log"
 	"time"
 )
 
 // Block keeps block headers
 type Block struct {
 	Timestamp     int64
-	Data          []byte
+	Transactions  []*Transaction
 	PrevBlockHash []byte
 	Hash          []byte
 	Nonce         int
-}
-
-// NewBlock creates and returns Block
-func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
-	pow := NewProofOfWork(block)
-	nonce, hash := pow.Run()
-
-	block.Hash = hash[:]
-	block.Nonce = nonce
-
-	return block
-}
-
-// NewGenesisBlock creates and returns genesis Block
-func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Block", []byte{})
 }
 
 // Serialize serializes the block
@@ -45,14 +30,43 @@ func (b *Block) Serialize() []byte {
 	return result.Bytes()
 }
 
-// Deserialize deserializes the block
+func (b *Block) HashTransaction() []byte {
+	var txHashes [][]byte
+	var txHash [32]byte
+
+	for _, tx := range b.Transactions {
+		txHashes = append(txHashes, tx.ID)
+	}
+	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
+
+	return txHash[:]
+}
+
+// NewBlock creates and returns Block
+func NewBlock(transactions []*Transaction, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), transactions, prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
+	return block
+}
+
+// NewGenesisBlock creates and returns genesis Block
+func NewGenesisBlock(coinbase *Transaction) *Block {
+	return NewBlock([]*Transaction{coinbase}, []byte{})
+}
+
+// DeserializeBlock deserializes a block
 func DeserializeBlock(d []byte) *Block {
 	var block Block
-	decoder := gob.NewDecoder(bytes.NewReader(d))
 
+	decoder := gob.NewDecoder(bytes.NewReader(d))
 	err := decoder.Decode(&block)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 
 	return &block
